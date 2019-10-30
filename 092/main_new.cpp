b29b69f5309ee2270730604e9e4ed22544aa7657
sky-bro <sky_bro@qq.com>
2019-10-30 12:47:38 +0000

    add main_new.cpp using Computer ID

--- 092/main_new.cpp
+++ 092/main_new.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
-#include <cstdint> // 包含这个然后使用uint64之类的更好
+#include <cstdint> // 包含这个, 使用uint64_t, uint32_t的更好
 #include "blowfish.h"
 
 using namespace std;
@@ -17,19 +17,25 @@ void printBlock(unsigned char *plaintext)
 
 int main(int argc, char const *argv[])
 {
-    char k0[30] = "76481-640-8834005-23362";
-    // char k0[30] = "94940361391";
-    printf("Please input your ProductID:\n");
-    scanf("%s", k0);
+    unsigned char plaintext[] = "fishblow";
+    printf("Please input your Computer ID:\n");
+    *(uint64_t*)(plaintext) = 0x173B565A8A0E6E53;
+    // 这里用lX可能会出错，表示unsigned long，linux 64位下，long为64bit，windows下32bit
+    // scanf("%08lX%08lX", (unsigned long *)(plaintext+4), (unsigned long*)(plaintext));
+
+    // 64bit (g++ -m64) 下可以这样
+    // scanf("%016lX", (unsigned long *)(plaintext));
+
+    // 更好的方式是
+    // scanf("%08X%08X", (unsigned int*)(plaintext+4), (unsigned int*)(plaintext));
+    scanf("%016llX", (uint64_t*)(plaintext));
+
     char k1[] = "ChinaCrackingGroup";
     char k2[] = "CrackingForFun";
-    auto cipher = Blowfish(vector<char>(k0, k0 + strlen(k0)));
-    unsigned char plaintext[] = "fishblow";
-    cipher.EncryptBlock((uint32_t *)(plaintext + 4), (uint32_t *)(plaintext));
 
     printBlock(plaintext);
 
-    cipher = Blowfish(vector<char>(k1, k1 + strlen(k1)));
+    auto cipher = Blowfish(vector<char>(k1, k1 + strlen(k1)));
     cipher.EncryptBlock((uint32_t *)(plaintext + 4), (uint32_t *)(plaintext));
 
     printBlock(plaintext);
